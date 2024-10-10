0d1073e845ef9da7e910daf408abd48087790e1b
Bruno Nascimento <brunonascimentodev@gmail.com>
2024-10-10 13:52:28 -0300

    clean old pkgs folders

--- polygon-bor/default.nix
+++
@@ -1,7 +0,0 @@
-let
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05";
-  pkgs = import nixpkgs { config = {}; overlays = []; };
-in
-{
-  polygon-bor = pkgs.callPackage ./polygon-bor.nix { inherit (pkgs.darwin) IOKit libobjc; };
-}
--- polygon-heimdall/default.nix
+++
@@ -1,7 +0,0 @@
-let
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05";
-  pkgs = import nixpkgs { config = {}; overlays = []; };
-in
-{
-  polygon-heimdall = pkgs.callPackage ./polygon-heimdall.nix { inherit (pkgs.darwin) IOKit libobjc; };
-}
