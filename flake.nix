8780928470cacea5a11944239f571252618081bf
Bruno Nascimento <brunonascimentodev@gmail.com>
2024-10-25 00:29:44 -0300

    add lib import on packages

--- flake.nix
+++ flake.nix
@@ -5,8 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs";
   };
 
-  outputs =
-    { self, nixpkgs, ... }:
+  outputs = { self, nixpkgs, ... }:
     let
       # Supported architectures: x86_64 and aarch64
       supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
@@ -19,8 +18,8 @@
           pkgs = nixpkgsFor.${system};
         in
         {
-          bor = import ./pkgs/bor/default.nix { inherit self nixpkgs pkgs; };
-          heimdall = import ./pkgs/heimdall/default.nix { inherit self nixpkgs pkgs; };
+          bor = import ./pkgs/bor/default.nix { inherit self nixpkgs pkgs; lib = pkgs.lib; };
+          heimdall = import ./pkgs/heimdall/default.nix { inherit self nixpkgs pkgs; lib = pkgs.lib; };
         }
       );
 
