458c8e42efaeaa93149f35aa3260aec8150abfdd
Bruno Nascimento <brunonascimentodev@gmail.com>
2024-10-25 00:31:29 -0300

    add all necessary arguments

--- flake.nix
+++ flake.nix
@@ -10,16 +10,35 @@
       # Supported architectures: x86_64 and aarch64
       supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      # Import Nixpkgs for all systems
       nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
     in
     {
+      # Define the packages for all systems
       packages = forAllSystems (system:
         let
           pkgs = nixpkgsFor.${system};
-        in
-        {
-          bor = import ./pkgs/bor/default.nix { inherit self nixpkgs pkgs; lib = pkgs.lib; };
-          heimdall = import ./pkgs/heimdall/default.nix { inherit self nixpkgs pkgs; lib = pkgs.lib; };
+        in {
+          bor = import ./pkgs/bor/default.nix {
+            inherit self nixpkgs pkgs;
+            lib = pkgs.lib;
+            stdenv = pkgs.stdenv;
+            buildGoModule = pkgs.buildGoModule;
+            fetchFromGitHub = pkgs.fetchFromGitHub;
+            libobjc = pkgs.darwin.libobjc;
+            IOKit = pkgs.darwin.IOKit;
+          };
+
+          heimdall = import ./pkgs/heimdall/default.nix {
+            inherit self nixpkgs pkgs;
+            lib = pkgs.lib;
+            stdenv = pkgs.stdenv;
+            buildGoModule = pkgs.buildGoModule;
+            fetchFromGitHub = pkgs.fetchFromGitHub;
+            libobjc = pkgs.darwin.libobjc;
+            IOKit = pkgs.darwin.IOKit;
+          };
         }
       );
 
