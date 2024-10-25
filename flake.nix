265e1b25c0430e51a3ff98a3ec9c8e383d47e180
Bruno Nascimento <brunonascimentodev@gmail.com>
2024-10-25 00:09:13 -0300

    add direct reference to files on flakes

--- flake.nix
+++ flake.nix
@@ -5,27 +5,58 @@
     nixpkgs.url = "github:NixOS/nixpkgs";
   };
 
-  outputs =
-    { self, nixpkgs, ... }:
-    let
-      # Supported architectures: x86_64 and aarch64
-      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
-    in
-    {
-      packages = forAllSystems (system:
-        let
-          pkgs = nixpkgsFor.${system};
-        in
-        {
-          bor = import ./pkgs/bor/default.nix { inherit self nixpkgs pkgs; };
-          heimdall = import ./pkgs/heimdall/default.nix { inherit self nixpkgs pkgs; };
-        }
-      );
+  outputs = { self, nixpkgs, ... }: 
+  let
+    # Define supported systems
+    supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
 
-      # NixOS modules output
-      nixosModules.bor = import ./modules/bor/default.nix;
-      nixosModules.heimdall = import ./modules/heimdall/default.nix;
+    # Nixpkgs for each system
+    nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+
+    # Define the per-system package logic
+    perSystem = {
+      self',
+      pkgs,
+      system,
+      ...
+    }: let
+      inherit (pkgs) callPackage;
+      inherit (lib.extras.flakes) platformPkgs platformApps;
+    in {
+      packages = platformPkgs system rec {
+        bor = callPackage ./pkgs/bor { inherit (pkgs.darwin) IOKit libobjc; };
+        heimdall = callPackage ./pkgs/heimdall { inherit (pkgs.darwin) IOKit libobjc; };
+      };
+
+      apps = platformApps self'.packages {
+        bor = { bin = "bor"; };
+        heimdall = {
+          bin = "heimdalld";
+          heimdallcli.bin = "heimdallcli";
+        };
+      };
     };
+  
+  in {
+    # Define the overlay to expose packages
+    flake.overlays.default = _final: prev: let
+      inherit (prev.stdenv.hostPlatform) system;
+    in
+      if builtins.hasAttr system self.packages
+      then self.packages.${system}
+      else {};
+
+    # Expose packages for each system
+    packages = forAllSystems (system:
+      let pkgs = nixpkgsFor.${system};
+      in perSystem { self' = self; pkgs = pkgs; inherit system; }
+    );
+
+    # Expose NixOS modules
+    nixosModules = {
+      bor = import ./modules/bor/default.nix;
+      heimdall = import ./modules/heimdall/default.nix;
+    };
+  };
 }
